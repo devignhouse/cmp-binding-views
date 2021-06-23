32ddd34c119fab97c4ba6e341b4851b3935d900e
Nabiel Firfirey <devignhouse@gmail.com>
2021-06-23 12:35:04 +0200

    Assigning an alias to Custom Events

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  // Binding to Custom events
+  // @Input() element: { type: string, name: string, content: string };
+  // also you can
+  // Assign an alias for Custom Events as per below
+  @Input('srvElement') element: { type: string, name: string, content: string };
+
   constructor() { }
 
   ngOnInit(): void {
